910ef4323a96aa06262a7a157c273df4d1996f00
wangyaoyong <yaoyong.oerv@isrc.iscas.ac.cn>
2024-03-10 01:10:07 +0800

    support add and sub op

--- main.c
+++ main.c
@@ -2,14 +2,29 @@
 #include <stdlib.h>
 
 int main(int Argc, char** Argv) {
-    if (Argc != 2) {
-        fprintf(stderr, "%s: invalid number of arguments\n", Argv[0]);
-        return 1;
-    }
+  if (Argc != 2) {
+      fprintf(stderr, "%s: invalid number of arguments\n", Argv[0]);
+      return 1;
+  }
+  
+  char* P = Argv[1];
 
-    printf(".global main\n");
-    printf("main:\n");
-    printf("li a0, %d\n", atoi(Argv[1]));
-    printf("ret\n");
-    return 0;
+  printf(".global main\n");
+  printf("main:\n");
+
+  printf("  li a0, %ld\n", strtol(P, &P, 10));
+  while (*P) {
+    if (*P == '+') {
+      ++P;
+      printf("  addi a0, a0, %ld\n", strtol(P, &P, 10));
+    }
+    if (*P == '-') {
+      ++P;
+      printf("  addi a0, a0, -%ld\n", strtol(P, &P, 10));
+      continue;
+    }
+  }
+
+  printf("  ret\n");
+  return 0;
 }
